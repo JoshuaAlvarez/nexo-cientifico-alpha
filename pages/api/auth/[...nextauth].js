11cabdcc21c63109a08534f70c55bea983f3e8e7
JoshuaAlvarez <31878618+JoshuaAlvarez@users.noreply.github.com>
2022-11-30 16:54:44 -0600

    Update [...nextauth].js
    Updated nextauth.js function

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,17 +3,6 @@ import GoogleProvider from "next-auth/providers/google"
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "../../../lib/mongodb"
 
-// export const authOptions = {
-//   // Configure one or more authentication providers
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//     // ...add more providers here
-//   ],
-// }
-
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default NextAuth({
